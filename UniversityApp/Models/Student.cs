3d85b1a64a57676e9cf6d46cece21f67edfcc210
David Pineda <mr.davidpineda@gmail.com>
2017-10-18 16:03:35 -0700

    Added class files.

--- UniversityApp/Models/Student.cs
+++ UniversityApp/Models/Student.cs
@@ -1,15 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace UniversityApp.Models
+namespace ContosoUniversity.Models
 {
     public class Student
     {
         public int ID { get; set; }
         public string LastName { get; set; }
-        public string FirstName { get; set; }
+        public string FirstMidName { get; set; }
         public DateTime EnrollmentDate { get; set; }
 
         public ICollection<Enrollment> Enrollments { get; set; }
