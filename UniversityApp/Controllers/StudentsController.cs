3957b4ab657438598d51f5ab6fab8acdd139622a
David Pineda <mr.davidpineda@gmail.com>
2017-12-13 08:39:15 -0800

    Revert "Added functionality to index page"
    This reverts commit b6679ac5c38bd9e2a17abd45acec83357d5987e9.

--- UniversityApp/Controllers/StudentsController.cs
+++ UniversityApp/Controllers/StudentsController.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ContosoUniversity.Data;
 using ContosoUniversity.Models;
-using ContosoUniversity;
 
 namespace UniversityApp.Controllers
 {
