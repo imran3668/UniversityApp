db9826962dcf3a883716d8df1a2068b74f7ef09a
David Pineda <mr.davidpineda@gmail.com>
2017-12-13 21:33:32 -0800

    Updated view, models, and controllers

--- UniversityApp/Migrations/20171214052711_Inheritance.Designer.cs
+++ UniversityApp/Migrations/20171214052711_Inheritance.Designer.cs
@@ -13,9 +13,10 @@ using System;
 namespace UniversityApp.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20171214052711_Inheritance")]
+    partial class Inheritance
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
